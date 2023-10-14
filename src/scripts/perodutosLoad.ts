683378a69eaaee973bcbcc443271c48f6119c996
Marciel404 <103785110+Marciel404@users.noreply.github.com>
2023-10-14 11:35:32 -0300

    Update perodutosLoad.ts

--- src/scripts/perodutosLoad.ts
+++ src/scripts/perodutosLoad.ts
@@ -1,5 +1,4 @@
 import React from "react"
-import { getPordutosLoja } from "@/scripts/db/dbconections"
 
 export default function () {
     React.useEffect(() => {
@@ -7,41 +6,45 @@ export default function () {
         fetch("/api/getprodutos", { method: "GET" })
             .then(r => r.json())
             .then((r: any) => {
-                for (const i of JSON.parse(r.content)) {
-                    if (i._id != "quantidadeProdutos") {
-                        const div = document.createElement("div")
-                        const div2 = document.createElement("div")
-                        const button = document.createElement("button")
-                        const imgElement = document.createElement("img")
-                        const h4Element = document.createElement("h2")
-                        const paragraphElement = document.createElement("p")
-                        const h42Element = document.createElement("h4")
+                if (r.status === 400 || r.status === 405) {
+                    alert(r.error)
+                } else {
+                    for (const i of JSON.parse(r.content)) {
+                        if (i._id != "quantidadeProdutos") {
+                            const div = document.createElement("div")
+                            const div2 = document.createElement("div")
+                            const button = document.createElement("button")
+                            const imgElement = document.createElement("img")
+                            const h4Element = document.createElement("h2")
+                            const paragraphElement = document.createElement("p")
+                            const h42Element = document.createElement("h4")
 
-                        imgElement.src = i.urlIMAGE
-                        imgElement.id = "imageProd"
+                            imgElement.src = i.urlIMAGE
+                            imgElement.id = "imageProd"
 
-                        h4Element.textContent = i.nome
-                        paragraphElement.textContent = i.description
-                        h42Element.textContent = `R$ ${i.valor}`
+                            h4Element.textContent = i.nome
+                            paragraphElement.textContent = i.description
+                            h42Element.textContent = `R$ ${i.valor}`
 
-                        button.textContent = "Comprar"
+                            button.textContent = "Comprar"
 
-                        div.id = `produto-${i._id}`
-                        div.className = "divProduto"
-                        div2.className = "divProdutoText"
-                        button.id = "buttonBuy"
+                            div.id = `produto-${i._id}`
+                            div.className = "divProduto"
+                            div2.className = "divProdutoText"
+                            button.id = "buttonBuy"
 
-                        div.append(imgElement)
-                        div2.append(h4Element)
-                        div2.append(paragraphElement)
-                        div2.append(h42Element)
-                        div2.append(button)
-                        div.append(div2)
+                            div.append(imgElement)
+                            div2.append(h4Element)
+                            div2.append(paragraphElement)
+                            div2.append(h42Element)
+                            div2.append(button)
+                            div.append(div2)
 
-                        divprodutos?.appendChild(div)
+                            divprodutos?.appendChild(div)
+                        }
                     }
                 }
             })
-            .catch(e => {console.log(e)})
+            .catch(e => { console.log(e) })
     })
 }
