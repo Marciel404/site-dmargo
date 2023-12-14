c9b700a0f3960da6c55c98ee74aa49d77be0e445
Marciel404 <103785110+Marciel404@users.noreply.github.com>
2023-12-13 21:48:15 -0300

    Deleting unused file

--- src/scripts/dbconections.ts
+++
@@ -1,9 +0,0 @@
-import { MongoClient } from "mongodb";
-
-const cluster = new MongoClient(process.env.MONGOKET!)
-const db = cluster.db("MargoStore")
-const dbProdutos = db.collection("produtos")
-
-export function addProduto(name: string, imgURL: string, price: number, description: string){
-    
-}
